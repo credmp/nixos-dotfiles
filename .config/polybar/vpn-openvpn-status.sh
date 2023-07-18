f15a2e6bf2200a45aab928764623ca5822ef6b70
Arjen Wiersma <arjen@wiersma.org>
2023-07-18 13:38:09 +0200

    fix: show VPN shields instead of words

--- .config/polybar/vpn-openvpn-status.sh
+++ .config/polybar/vpn-openvpn-status.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-printf "VPN: " && (pgrep -a openvpn$ | head -n 1 | awk '{print $NF }' | cut -d '.' -f 1 && echo down) | head -n 1
+count=$(pgrep -a openvpn$ | head -n 1 |wc -l)
+
+if [[ "$count" == "1" ]]; then
+  echo "";
+else
+  echo "";
+fi
+
+#printf "" && (pgrep -a openvpn$ | head -n 1 | awk '{print $NF }' | cut -d '.' -f 1 && echo "") | echo ""
