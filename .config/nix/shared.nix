d869d7ee4a1a2f7be44b307d93013177d830e03e
Arjen Wiersma <arjen@wiersma.org>
2025-01-05 22:22:41 +0100

    feat: enabling gnome

--- .config/nix/shared.nix
+++ .config/nix/shared.nix
@@ -75,24 +75,24 @@
        xterm.enable = false;
      };
    
-     #displayManager.gdm.enable = true;
+     displayManager.gdm.enable = true;
      #displayManager.gdm.wayland = true;
      #windowManager.hypr.enable = true;
-     #desktopManager.plasma6.enable = true;
-     #desktopManager.gnome.enable = true;
+     # desktopManager.plasma6.enable = true;
+     desktopManager.gnome.enable = true;
 
-     #windowManager.i3 = {
-     #  enable = true;
-     #  extraPackages = with pkgs; [
-     #    dmenu #application launcher most people use
-     #    i3status # gives you the default i3 status bar
-     #    i3lock #default i3 screen locker
-     #    i3blocks #if you are planning on using i3blocks over i3status
-     #  ];
-     #};
+   #  windowManager.i3 = {
+   #    enable = true;
+   #    extraPackages = with pkgs; [
+   #      dmenu #application launcher most people use
+   #      i3status # gives you the default i3 status bar
+   #      i3lock #default i3 screen locker
+   #      i3blocks #if you are planning on using i3blocks over i3status
+   #    ];
+   #  };
    };
-   services.displayManager.sddm.enable = true;
-   services.desktopManager.plasma6.enable = true;
+   #services.displayManager.sddm.enable = true;
+   #services.desktopManager.plasma6.enable = true;
 
    # # -- hyprland
    # programs.hyprland = {
@@ -107,14 +107,14 @@
    #services.displayManager = {
    #    defaultSession = "none+i3";
    #};
-   #programs.xwayland.enable = true;
+   programs.xwayland.enable = true;
    xdg.portal.enable = true;
    xdg.portal.config.common.default = "*";
 
    xdg.portal.xdgOpenUsePortal = true;
    xdg.portal.extraPortals = [
-           pkgs.xdg-desktop-portal-gtk
-	   #pkgs.xdg-desktop-portal-gnome
+           #pkgs.xdg-desktop-portal-gtk
+	   pkgs.xdg-desktop-portal-gnome
 	   pkgs.xdg-desktop-portal-wlr
    ];
 
