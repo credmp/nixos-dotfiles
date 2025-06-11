f15a9712da2f57e274e6efc6b1bcb6a7acb1e88c
Arjen Wiersma <arjen@wiersma.org>
2025-06-11 15:14:34 +0200

    feat: clojure-lsp

--- .config/doom/custom.el
+++ .config/doom/custom.el
@@ -5,14 +5,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(safe-local-variable-values
-<<<<<<< HEAD
-   '((eval progn (setenv "DATABASE_URL" "jdbc:postgresql://localhost:5432/comics")
-=======
    '((cider-clojure-cli-aliases . ":dev") (cider-shadow-default-options . "app")
      (cider-default-cljs-repl . shadow)
      (eval progn
       (setenv "DATABASE_URL" "jdbc:postgresql://localhost:5432/comics")
->>>>>>> 99a2834 (feat: clojure-lsp)
       (setenv "DATABASE_USER" "postgres") (setenv "DATABASE_PASS" "test"))
      (lsp-ltex-language . "nl") (lsp-ltex-language . nl-NL)
      (lsp-ltex-language . "nl-NL") (ispell-dictionary . "nl_NL")
